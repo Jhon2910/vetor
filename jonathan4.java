e0fbe08dc36245a2ba3a88fa6abb595f51ceb3fb
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-24 08:41:13 -0300

    Create jonathan4.java

---
+++ jonathan4.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class jonathan4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i;
+
+        int[] VetorOriginal = new int[10];
+        int[] VetorInvertido = new int[10];
+
+        for (i = 0; i <= 9; i++) {
+            System.out.printf("Digite %d° numero do Vetor Original: ", (i + 1));
+            while (!sc.hasNextInt()) {
+                System.out.println("Tente novamente!Digite apenas números inteiros!");
+                System.out.printf("Digite %d° numero do Vetor Original: ", (i + 1));
+            }
+            VetorOriginal[i] = sc.nextInt();
+        }
+
+        System.out.println(" ");
+
+        int i1 = 0, j1 = 9 ;
+
+        while (i1<=9 && j1 >= 0){
+            VetorInvertido[i1] = VetorOriginal[j1];
+            i1++;
+            j1--;
+        }
+
+        System.out.println("Vetor Original = [ ");
+        for (i = 0; i <= 9; i++) {
+            System.out.printf(" %d ",VetorOriginal[i]);
+        }
+        System.out.print(" ]");
+
+        System.out.println(" ");
+
+        System.out.print("\nVetor Invertido = [ ");
+        for (i = 0; i <= 9; i++) {
+            System.out.printf(" %d ",VetorInvertido[i]);
+        }
+        System.out.print(" ]");
+        sc.close();
+    }
+
+}
