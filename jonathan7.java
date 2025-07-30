3a92310f884da3e6dbbb0da8ec58953b8c4c3ae6
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-29 22:07:14 -0300

    Update jonathan7.java

--- jonathan7.java
+++ jonathan7.java
@@ -3,52 +3,52 @@ import java.util.Scanner;
 public class jonathan7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, par = 0, impar = 0;
-        int i = 0;
+        int n, nulo = 0, par = 0, impar = 0;
+        int i;
         double div;
 
-        while (i < Integer.MAX_VALUE) {
-
+        System.out.print("Digite quantos numeros gostaria de digitar: ");
+        while (!sc.hasNextInt()) {
+            System.out.print("Digite apenas números inteiros!");
             System.out.print("Digite quantos numeros gostaria de digitar: ");
-            while (!sc.hasNextInt()) {
-                System.out.print("Digite apenas números inteiros!");
-                System.out.print("Digite quantos numeros gostaria de digitar: ");
-                sc.next();
-            }
-            n = sc.nextInt();
-
-            int vet[] = new int[n];
-            int vet1[] = new int[n];
-            int vet2[] = new int[n];
-
-            for (i = 0; i < n; i++) {
-                System.out.printf("Digite o %d° numero até 100: ", i + 1);
-                vet[i] = sc.nextInt();
-
-                if (vet[i] <= 100) {
-
-                    if (vet[i] % 2 == 0 || vet[i] >= 0) {
-                        vet1[i] = vet[i];
-                        if (i < n - 2) {
-                            par = vet1[i] + vet1[i + 1];
-                        }
-                    } else {
-                        vet2[i] = vet[i];
-                        if (i < n - 2) {
-                            impar = vet2[i] + vet2[i + 1];
-                        }
-                    }
-
-                    div = (double) par / (double) impar;
-
-                    System.out.print(div);
-                }
-                else{
-                    System.out.println("Digite um número até 100!");
-                    sc.next();
-                }
-            }
-            sc.close();
+            sc.next();
         }
+        n = sc.nextInt();
+
+        int vet[] = new int[n];
+        int vet1[] = new int[n];
+        int vet2[] = new int[n];
+
+        for (i = 0; i < n; i++) {
+            System.out.printf("Digite o %d° numero até 100: ", i + 1);
+            vet[i] = sc.nextInt();
+
+            if (vet[i] <= 100 && sc.hasNextInt()) {
+                if (vet[i] == 0) {
+                    vet1[i] = nulo;
+                } else if (vet1[i] % 2 != 0 || vet1[i] < 0) {
+                    vet2[i] = vet[i];
+                }else {
+                    vet1[i] = vet[i];
+                }
+
+            } else {
+                if (!sc.hasNextInt()) {
+                    System.out.println("Digite apenas numeros inteiros!");
+                } else {
+                    System.out.println("Digite um número até 100!");
+                }
+                sc.next();
+                i--;
+            }
+        }
+        for (i=0;i < n-1;i++){
+            par = vet1[i] + vet1[i+1];
+            impar = vet2[i] + vet2[i+1];
+        }
+        div = (double) par / (double) impar;
+
+        System.out.print(div);
+        sc.close();
     }
 }
