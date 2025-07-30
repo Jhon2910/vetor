1ae989807d6b9776807b8b0fb16c3a5c79dae9f7
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-30 20:50:11 -0300

    Update jonathan8.java

--- jonathan8.java
+++ jonathan8.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class jonathan8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String porcento = "%";
         int i,j, valor;
-        int notaDoAluno[] = new int[22];
+        int notaDoAluno[] = new int[80];
         int absoluta[] = new int[11];
         double relativa[] = new double[11];
-        boolean erro = true;
 
-        while (true) {
             for (i = 0; i < notaDoAluno.length; i++) {
                 System.out.printf("\nDigite a nota do aluno %d: ", i + 1);
                 while (!sc.hasNextInt()) {
@@ -23,7 +22,6 @@ public class jonathan8 {
                 if (valor < 0 || valor > 10) {
                     System.out.print("\nErro!Digite uma nota entre e 0 e 10! ");
                     i--;
-                    erro = false;
                 } else {
                     for (j = 0; j <= 10; j++) {
                         if (valor == j) {
@@ -39,11 +37,9 @@ public class jonathan8 {
             System.out.println("Nota                    Frequência Absoluta              Frequência Relativa");
             for (i = 0; i <= 10; i++) {
                 System.out.println("\n------------------------------------------------------------------------");
-                System.out.printf("\n%d                         %d                                 %d", i, absoluta[i], (int) (relativa[i] * 100));
+                System.out.printf("\n%d                         %d                                 %.2f%s", i, absoluta[i],(relativa[i] * 100),porcento);
             }
             System.out.println("\n-------------------------------------------------------------------------");
-            return;
         }
 
     }
-}
