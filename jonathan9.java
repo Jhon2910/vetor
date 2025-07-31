b46ed41dff3fe4d86d78ba0e58226219740d3c66
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-31 18:46:53 -0300

    Update jonathan9.java

--- jonathan9.java
+++ jonathan9.java
@@ -1,89 +1,67 @@
-
 import java.util.Scanner;
 
 public class jonathan9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int fichas[] = new int[5];
-        double alturageral[] = new double[5];
-        double alturaH = 0;
-        double alturaM = 0;
+
+        int alunos[] = new int[5];
+        int i, j;
+        double altura[] = new double[5];
         int sexo[] = new int[5];
-        int i;
-        int codigo[] = new int[5];
-        double x = 0, y = 0;
-        double media = 0, mediaH = 0, mediaM = 0;
-        double maior = Integer.MIN_VALUE;
-        double menor = Integer.MAX_VALUE;
-        int quantM = 0;
-        int quantH = 0;
+        double maior, menor, aux;
         double soma = 0;
 
-        for (i = 0; i < alturageral.length; i++) {
-            System.out.printf("\nDigite a alturageral do aluno %d em metros : ", i + 1);
+        for (i = 0; i < altura.length; i++) {
+            System.out.printf("\nDigite a altura do aluno %d em metros : ", i + 1);
             while (!sc.hasNextDouble()) {
                 System.out.println("\nDigite um numero valido! ");
-                System.out.printf("\nDigite a alturageral do aluno %d em metros : ", i + 1);
+                System.out.printf("\nDigite a altura do aluno %d em metros : ", i + 1);
                 sc.next();
             }
-            alturageral[i] = sc.nextDouble();
-
-            if (alturageral[i] > maior) {
-                maior = alturageral[i];
-            }
-            if (alturageral[i] < menor) {
-                menor = alturageral[i];
-            }
-
-            if (alturageral[i] < 0) {
-                System.out.print("Digite um numero positivo!");
+            altura[i] = sc.nextDouble();
+            if (altura[i] < 0) {
+                System.out.print("Digite um valor positivo!");
                 i--;
-            } else {
-                System.out.print("Digite o sexo (1 para masculino e 2 para feminino): ");
-                while (!sc.hasNextInt()) {
-                    System.out.print("Digite um numero inteiro!");
-                    System.out.print("Digite o sexo (1 para masculino e 2 para feminino): ");
-                    sc.next();
-                }
-                sexo[i] = sc.nextInt();
-
-                if (sexo[i] < 1 || sexo[i] > 2) {
-                    System.out.print("Digite apenas 1 ou 2!");
-                    i--;
-                } else {
-                    if (sexo[i] == 1) {
-                        quantH += 1;
-                        alturaH += alturageral[i];
-                        mediaH = alturaH / quantH;
-                        if (alturageral[i] < mediaM) {
-                            double abaixo[] = new double[quantH];
-                            for (int j = 0; j < quantH; j++) {
-                                abaixo[j] = alturageral[i];
-                            }
-                        }
-                    } else {
-                        quantM += 1;
-                        alturaM += alturageral[i];
-                        if (alturaM[i] > mediaH) {
-                            
-                        }
-                    }
-                   
-                    mediaM = alturaM / quantM;
-                }
             }
-            soma += alturageral[i];
         }
 
-        media = soma / alturageral.length;
-        
-        System.out.printf("A maior alturageral é : %.2f", maior);
-        System.out.printf("\nA menor alturageral é : %.2f", menor);
-        System.out.printf("\nA média é :%.2f: ", media);
-        System.out.printf("\nAs mulheres com a alturageral acima da media da alturageral dos homens: %.2f", acima);
-        System.out.printf("\nOs homens com a alturageral abaixo da media das mulheres: %.2f", abaixo[j]);
+        for (i = 0; i < sexo.length; i++) {
+            System.out.printf("\nDigite o sexo do aluno %d(1 para masculino e 2 para feminino): ", i + 1);
+            while (!sc.hasNextInt()) {
+                System.out.print("\nDigite um numero inteiro!");
+                System.out.printf("\nDigite o sexo do aluno %d(1 para masculino e 2 para feminino): ", i + 1);
+                sc.next();
+            }
+            sexo[i] = sc.nextInt();
+            if (sexo[i] < 1 && sexo[i] > 2) {
+                System.out.print("\nDigite apenas 1 ou 2!");
+                i--;
+            }
+        }
 
-        sc.close();
+        for (i = 0; i < alunos.length - 1; i++) {
+            for (j = 0; j < alunos.length - 1 - i; j++) {
+                if (altura[j] > altura[j + 1]) {
+                    aux = altura[j];
+                    altura[j] = altura[j + 1];
+                    altura[j + 1] = aux;
+                }
+            }
+        }
+
+        for (i = 0; i < alunos.length; i++) {
+            soma += alunos[i];
+        }
+
+        double media = soma/args.length;
+
+        alturas(altura,media);
+    }
+
+    public static void alturas(double []altura,double media){
+        System.out.printf("\nA menor altura e : %.2f", altura[0]);
+        System.out.printf("\nA maior altura e : %.2f", altura[altura.length - 1]);
+        System.out.printf("\nA media e : %.2f", media);
     }
 }
