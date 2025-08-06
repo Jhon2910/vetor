de1bd3cea6834925873d093c1a0281d1c3270d59
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-08-06 12:20:31 -0300

    Create jonathan16.java

---
+++ jonathan16.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class jonathan16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N;
+        N = sc.nextInt();
+        int medidas[]  = new int[N];
+        boolean pico = false;
+        for (int i=0;i < ){
+            medidas
+        }
+        
+    }
+}
