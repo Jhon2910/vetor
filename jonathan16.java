db66a7255c03dc858e53c449a9a667da1ef0b5c5
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-08-11 16:40:59 -0300

    Update jonathan16.java

--- jonathan16.java
+++ jonathan16.java
@@ -1,16 +1,43 @@
 import java.util.Scanner;
 
 public class jonathan16 {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N;
-        N = sc.nextInt();
-        int medidas[]  = new int[N];
-        boolean pico = false;
-        for (int i=0;i < ){
-            medidas[j] = sc.nextInt();
+        System.out.print("Digite o tamanho da sequência: ");
+        while (!sc.hasNextInt()) {
+            System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+            System.out.print("\nDigite o tamanho da sequência: ");
+            sc.next();
         }
+        int N = sc.nextInt();
 
+        System.out.println(" ");
+
+        int A[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            System.out.printf("Digite o %d numero: ", i + 1);
+            while (!sc.hasNextInt()) {
+                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                System.out.printf("\nDigite o %d numero: ", i + 1);
+                sc.next();
+            }
+            A[i] = sc.nextInt();
+        }
+        saida(N, A);
+    }
+
+    public static void saida(int N, int A[]) {
+        String resultado = "N";
+
+        for (int i = 1; i < N - 1; i++) {
+            if (A[i - 1] > A[i] && A[i] < A[i + 1]) {
+                resultado = "S";
+                break;
+            }
+            System.out.printf("resultado:  %s ", resultado);
+        }
     }
 }
