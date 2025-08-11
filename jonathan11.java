895608712428b683b29b3c1c926fd5010422df67
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-08-11 16:55:12 -0300

    Update jonathan11.java

--- jonathan11.java
+++ jonathan11.java
@@ -1,1 +1,61 @@
+import java.util.Scanner;
 
+public class jonathan11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
+
+        System.out.print("Quantos numeros primos deseja encontrar? ");
+        int quantidadeDesejada = sc.nextInt();
+
+        System.out.print("Iniciar a busca a partir de qual numero? ");
+        int numeroDePartida = sc.nextInt();
+
+        System.out.println("\nIniciando a busca...");
+
+        int tempoInicial = (int) System.nanoTime();
+
+        int[] primosEncontrados = new int[quantidadeDesejada];
+        int quantidadeEncontrada = 0;
+        int numeroAtual = numeroDePartida;
+
+        while (quantidadeEncontrada < quantidadeDesejada) {
+            boolean ehPrimoDeFato = true;
+
+            if (numeroAtual <= 1) {
+                ehPrimoDeFato = false;
+            } else {
+                for (int divisor = 2; divisor * divisor <= numeroAtual; divisor++) {
+                    if (numeroAtual % divisor == 0) {
+                        ehPrimoDeFato = false;
+                        break;
+                    }
+                }
+            }
+
+            if (ehPrimoDeFato) {
+                primosEncontrados[quantidadeEncontrada] = numeroAtual;
+                quantidadeEncontrada++;
+            }
+            numeroAtual++;
+        }
+
+        int tempoFinal = (int) System.nanoTime();
+        int duracaoEmNanossegundos = tempoFinal - tempoInicial;
+
+        System.out.println("\nBusca finalizada! Os " + quantidadeDesejada + " primos encontrados foram:");
+
+        System.out.print("[");
+        for (int i = 0; i < primosEncontrados.length; i++) {
+            System.out.print(primosEncontrados[i]);
+            if (i < primosEncontrados.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+
+        System.out.println("\nTempo de execucao: " + duracaoEmNanossegundos + " nanossegundos.");
+
+        sc.close();
+    }
+}
